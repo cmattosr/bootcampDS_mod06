ab67ab85328658c9407f9e4ecec62278a9a84795
cmattosr <cmrocha@gmail.com>
2021-11-10 07:41:00 -0300

    pastas

---
+++ simulador_avaliacao_credito.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.image("images/bytebank_logo.png")
+st.write("# Simulador de Avaliação de Crédito")
+
+my_expander_1 = st.beta_expander("Trabalho")
+
+my_expander_2 = st.beta_expander("Pessoal")
+
+my_expander_3 = st.beta_expander("Família")
